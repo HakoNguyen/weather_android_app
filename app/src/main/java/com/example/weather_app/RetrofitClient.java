f54fa59106416abf31e9f2870fcffd2acd98eaaf
sangtd <sangtd2005@gmail.com>
2025-11-06 02:24:26 +0700

    upd, url api

--- app/src/main/java/com/example/weather_app/RetrofitClient.java
+++ app/src/main/java/com/example/weather_app/RetrofitClient.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit = null;
-    
-    private static final String BASE_URL = "https://weather.sangtd.workers.dev/";
+
+    private static final String BASE_URL = "https://lows-discipline-converter-livecam.trycloudflare.com/";
 
     public static WeatherService getApiService() {
         if (retrofit == null) {
